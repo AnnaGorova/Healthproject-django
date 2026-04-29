07b48e263d425ec701ac1ff60ce2d860a777ce95
Anna_Gorova <koshka20.03.19@gmail.com>
2026-04-29 23:42:49 +0300

    Html page - |upper, truncatewords:5, {% for %}

--- diary/urls.py
+++ diary/urls.py
@@ -4,9 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('old_home/', views.old_home, name='old_home'),
-    path('index/', views.index, name='index'),
     path('records/', views.records, name='records'),
-    path('record_detail/<int:record_id>/', views.record_detail, name='record_detail'),
+    path('record_detail/<int:pk>/', views.record_detail, name='record_detail'),
     path('medicines/', views.medicines, name='medicines'),
     path('profile/', views.profile, name='profile'),
     path('administrator/', views.administrator, name='administrator'),
--- diary/views.py
+++ diary/views.py
@@ -11,96 +11,68 @@ def old_home(request):
 
 
 def home(request):
-    return HttpResponse(
-        "<h2> Привіт, Django!</h2>"
-        
-        
-        "<h1><strong> Щоденник самопочуття </strong></h1>"
-    )
+    repo = get_repo_with_data()
+    user = repo.find_user(1)
+    records_count = len(repo.get_user_records(user.id))
 
+    context = {
+        'records_count': records_count,
+        'user': user,
+    }
 
-def index(request):
-    http_method = request.method
+    return render(request, 'diary/home.html', context)
 
-    repo = HealthRecordRepository()
-    user = repo.add_user("Olga Ivanova", "olga_I@gmail.com", "patient")
-
-    paracetamol = repo.add_medicine("Парацетамол", "500мг", "від температури")
-    amlodipine = repo.add_medicine("Амлодипін", "5мг", "від тиску")
-
-    record = repo.add_record(
-        user_id=user.id,
-        well_being="погано",
-        temperature=37.8,
-        pressure="130/80",
-        complaints="Головний біль",
-        comment="Прийняла ліки"
-    )
-
-    record.add_medicine(paracetamol)
-    record.add_medicine(amlodipine)
-    user_records = repo.get_user_records(user.id)
-
-    return HttpResponse(f"""
-        <h1>Щоденник самопочуття</h1>
-        <h3>HTTP Method: {http_method}</h3>
-        
-        <p><strong>Користувач:</strong> {user}</p>
-        <p><strong>Запис:</strong></p>
-        <p>{record}</p>
-        <p><strong>Записи за сьогодні:</strong> {len(user_records)}</p>
-        <hr>
-        <p>Django + HealthRecordRepository</p>
-    """)
 
 
 def records(request):
     http_method = request.method
 
-    repo = HealthRecordRepository()
-    user = repo.add_user("Olga Ivanova", "olga_I@gmail.com", "patient")
-    paracetamol = repo.add_medicine("Парацетамол", "500мг", "від температури")
-
-    record1 = repo.add_record(
-        user_id=user.id,
-        well_being="дуже погано",
-        temperature=38.6,
-        pressure="120/80",
-        complaints="Слабкість, температура",
-        comment="(((("
-    )
-    record1.add_medicine(paracetamol)
-
-    all_records = repo.get_user_records(user.id)
-
-    records_html = ""
-    for r in all_records:
-        records_html += f"<p>{r}</p><hr>"
-
-    return HttpResponse(f"""
-        <h1>Всі записи про самопочуття</h1>
-        <h3>Сторінка записів</h3>
-        <p>HTTP Method: {http_method}</p>
-        <h2>Записи користувача {user.username}:</h2>
-        {records_html}
-        
-        <p><strong> Всього записів: </strong> {len(all_records)}</p>
-        <hr>
-    
-    """)
-
-def record_detail(request, record_id):
     repo = get_repo_with_data()
 
-    for record in repo._records:
-        if record.id == record_id:
-            return HttpResponse(f"""
-                <h1>Запис №{record_id}</h1>
-                <p>{record}</p>
-                
-            """)
-    raise Http404("Запис не знайдено")
+    user = repo.find_user(1)
 
+    if user:
+        all_records = repo.get_user_records(user.id)
+    else:
+        all_records = []
+
+   
+    records_html = ""
+    for r in all_records:
+        records_html += f"<p>ID-{r.id} - {r}</p><hr>"
+
+    context = {
+        'records': all_records,
+        'user': user,
+       
+    }
+    
+    return render(request, 'diary/records.html', context)
+
+def record_detail(request, pk):
+    repo = get_repo_with_data()
+
+    record = None
+    for r in repo._records:
+        if r.id == pk:
+            record = r
+            break
+    
+
+    if not record:
+        raise Http404("Запис не знайдено")
+    
+
+    related_medicines = record.medicines
+   
+    
+    context = {
+        'record': record,
+        'related_medicines': related_medicines,  
+        'record_date': record.date
+    }
+
+    return render(request, 'diary/record_detail.html', context)
 
 
 
@@ -108,7 +80,7 @@ def medicines(request):
     http_method = request.method
     search = request.GET.get('search', '')
 
-    repo = HealthRecordRepository()
+    repo = get_repo_with_data()
 
     paracetamol = repo.add_medicine("Парацетамол", "500мг", "від температури")
     amlodipine = repo.add_medicine("Амлодипін", "5мг", "від тиску")
@@ -180,7 +152,8 @@ def create_test_data(repo: HealthRecordRepository):
         well_being="погано",
         temperature=37.8,
         pressure="130/80",
-        complaints="Головний біль",
+        complaints="Головний біль, температура 38.5, слабкість " \
+        "у всьому тілі, кашель, нежить, біль у горлі, запаморочення",
         comment="Прийняла ліки"
     )
     record1.add_medicine(paracetamol)
@@ -192,7 +165,8 @@ def create_test_data(repo: HealthRecordRepository):
         well_being="дуже погано",
         temperature=38.6,
         pressure="140/90",
-        complaints="Сильна слабкість, кашель",
+        complaints="Легке запаморочення при зміні положення тіла, шум у вухах, невелика слабкість у ногах, " \
+        "головний біль помірний в потиличній ділянці",
         comment="Звернулась до лікаря"
     )
     record2.add_medicine(paracetamol)
